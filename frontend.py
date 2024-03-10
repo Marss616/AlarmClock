12c3732267e912c147f9087f16de0abc5d68aba2
Marss616 <67536529+Marss616@users.noreply.github.com>
2024-03-10 17:53:54 +1100

    fixed syntax error

--- frontend.py
+++ frontend.py
@@ -10,7 +10,7 @@ root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Label(frm, input="").grid(column=0, row=1)
+ttk.Label(frm, text="").grid(column=0, row=1)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
 # Complete the front end for basic alarm clock with output of the time and date from the backend
 root.mainloop()

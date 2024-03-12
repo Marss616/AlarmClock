dfc9b39168e6f0a40bb1f5c87fcf8353ed8ad53c
Marss616 <67536529+Marss616@users.noreply.github.com>
2024-03-12 22:06:56 +1100

    added gettime function

--- backend.py
+++ backend.py
@@ -1,1 +1,9 @@
 # Contain functions for the application
+import datetime
+def gettime():
+    time = datetime.datetime.now().time()
+    date = datetime.datetime.now().date()
+    print('current time and date is:', time, date)
+    print()
+
+gettime()
--- frontend.py
+++ frontend.py
@@ -1,10 +1,11 @@
 # Contain GUI for the application
 
-from tkinter import *
-from tkinter import ttk
+from backend import *
 
 from tkinter import *
 from tkinter import ttk
+
+gettime()
 
 root = Tk()
 frm = ttk.Frame(root, padding=10)
@@ -14,4 +15,5 @@ ttk.Label(frm, text="").grid(column=0, row=1)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
 # Complete the front end for basic alarm clock with output of the time and date from the backend
 root.mainloop()
+
 
